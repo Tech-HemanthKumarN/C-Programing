af5ca85e7b5a4451e33b2a4b86886357224e7fed
Tech-HemanthKumarN <hemanthnanjappaji@gmail.com>
2024-10-31 17:52:02 +0530

    c++pro

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>// #-->means it is a preprocessor directive,it tells the compiler to include iostream header file
+// heder file provide the function and class declaration...
+using namespace std;// (stander)std contain cout ka defination..
+int main() //starting point of code
+{ //it define the scopeof code or programm,means what ever the things which return inside the flower bracket it belogs to it self only.
+//it make a group of code
+    cout<<"Hello Dunia"<<endl;// here "Hello world"--> it is a string
+    // endl-->means new line
+   
+    for(int i=0; i<6; i++) 
+    {
+        cout<<"Arya"<<endl;
+        // << --> which means insertion...
+        
+    }
+    return -1;//it represent successfull execution..
+    //if it return non zero unsucessfull execution.. 
+    // ;-->termination
+}
---
+++ 3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=20, b=30, res;
+    res=a*b;
+    cout<<res;
+}
+
