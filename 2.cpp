9fee5021300e4dcd15d3c1e4f5aae5feb8f038a6
Tech-HemanthKumarN <hemanthnanjappaji@gmail.com>
2024-10-31 17:46:20 +0530

    1st programming

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //variable define
+    int marks=50;//variable declartion
+   // variable named storage memory loction
+   cout<<" my marks is:"<<marks<<endl;
+   //variable declare
+   int age;
+   cout<<sizeof(age)<<" "<<endl;
+
+
+   
+    return 0;
+}
